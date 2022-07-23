1472ed4e00c42e046aa18998e85296f60b376c4b
Dreyk <i_khaustov@list.ru>
2022-07-23 10:20:36 +0700

    Range*

--- Range/src/ru/academits/khaustov/range/Range.java
+++ Range/src/ru/academits/khaustov/range/Range.java
@@ -32,4 +32,79 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    public double[] getIntervalsIntersection(Range range2) {
+        double from1 = from;
+        double to1 = to;
+        double from2 = range2.getFrom();
+        double to2 = range2.getTo();
+
+        if (to1 <= from2 || to2 <= from1) {
+            return null;
+        }
+
+        double[] intersection = new double[2];
+        intersection[0] = Math.max(from1, from2);
+        intersection[1] = Math.min(to1, to2);
+
+        return intersection;
+    }
+
+    public Range[] getIntervalsUnion(Range range1, Range range2) {
+        double from1 = range1.getFrom();
+        double to1 = range1.getTo();
+        double from2 = range2.getFrom();
+        double to2 = range2.getTo();
+        Range[] union;
+
+        if (to1 < from2 || to2 < from1) {
+            union = new Range[2];
+            union[0] = range1;
+            union[1] = range2;
+
+            return union;
+        }
+
+        union = new Range[1];
+        Range unionRange = new Range(Math.min(from1, from2), Math.max(to1, to2));
+        union[0] = unionRange;
+
+        return union;
+    }
+
+    public Range[] getIntervalsDifference(Range range1, Range range2) {
+        double from1 = range1.getFrom();
+        double to1 = range1.getTo();
+        double from2 = range2.getFrom();
+        double to2 = range2.getTo();
+        Range[] difference;
+        Range differenceRange;
+
+        if (to1 <= to2 && from1 >= from2) {
+            return null;
+        } else if (from1 < from2 && to1 <= to2) {
+            differenceRange = new Range(from1, from2);
+
+            difference = new Range[1];
+            difference[0] = differenceRange;
+
+            return difference;
+        } else if (from1 >= from2) {
+            differenceRange = new Range(to2, to1);
+
+            difference = new Range[1];
+            difference[0] = differenceRange;
+
+            return difference;
+        }
+
+        Range differenceRange1 = new Range(from1, from2);
+        Range differenceRange2 = new Range(to2, to1);
+
+        difference = new Range[2];
+        difference[0] = differenceRange1;
+        difference[1] = differenceRange2;
+
+        return difference;
+    }
 }
--- Range/src/ru/academits/khaustov/range_main/RangeMain.java
+++ Range/src/ru/academits/khaustov/range_main/RangeMain.java
@@ -2,37 +2,144 @@ package ru.academits.khaustov.range_main;
 
 import ru.academits.khaustov.range.Range;
 
+import java.util.Arrays;
+
 public class RangeMain {
     public static void main(String[] args) {
-        Range range = new Range(12.3, 16.7);
+        Range range1 = new Range(12.3, 16.7);
+        Range range2 = new Range(14.3, 15.9);
 
-        System.out.println("Начало диапазона: " + range.getFrom());
-        System.out.println("Конец диапазона: " + range.getTo());
+        System.out.println("Начало первого диапазона: " + range1.getFrom());
+        System.out.println("Конец первого диапазона: " + range1.getTo());
+        System.out.println("Начало второго диапазона: " + range2.getFrom());
+        System.out.println("Конец второго диапазона: " + range2.getTo());
 
-        System.out.println("Длина диапазона равна: " + range.getRangeLength());
+        System.out.println("Длина первого диапазона равна: " + range1.getRangeLength());
+        System.out.println("Длина второго диапазона равна: " + range2.getRangeLength());
 
-        double number = 14;
+        double number = 14.5;
 
-        if (range.isInside(number)) {
-            System.out.println("Число " + number + " принадлежит диапазону.");
+        if (range1.isInside(number)) {
+            System.out.println("Число " + number + " принадлежит первому диапазону");
         } else {
-            System.out.println("Число " + number + " не принадлежит диапазону.");
+            System.out.println("Число " + number + " не принадлежит первому диапазону");
+        }
+
+        if (range2.isInside(number)) {
+            System.out.println("Число " + number + " принадлежит второму диапазону");
+        } else {
+            System.out.println("Число " + number + " не принадлежит второму диапазону");
+        }
+
+        double[] intervalsIntersection = range1.getIntervalsIntersection(range2);
+
+        if (intervalsIntersection == null) {
+            System.out.println("Диапазоны не пересекаются");
+        } else {
+            System.out.println("Диапазоны пересекаются в точках: " + Arrays.toString(intervalsIntersection));
+        }
+
+        Range[] intervalsUnion = range1.getIntervalsUnion(range1, range2);
+
+        if (intervalsUnion.length == 1) {
+            double from = intervalsUnion[0].getFrom();
+            double to = intervalsUnion[0].getTo();
+
+            System.out.printf("Объединение двух диапазонов: [%f, %f]%n", from, to);
+        } else {
+            double from1 = intervalsUnion[0].getFrom();
+            double to1 = intervalsUnion[0].getTo();
+            double from2 = intervalsUnion[1].getFrom();
+            double to2 = intervalsUnion[1].getFrom();
+
+            System.out.printf("Объединение двух диапазонов: [%f, %f], [%f, %f]%n", from1, to1, from2, to2);
+        }
+
+        Range[] intervalsDifference = range1.getIntervalsDifference(range1, range2);
+
+        if (intervalsDifference == null) {
+            System.out.println("Разность диапазонов равна нулю");
+        } else if (intervalsDifference.length == 1) {
+            double from = intervalsDifference[0].getFrom();
+            double to = intervalsDifference[0].getTo();
+
+            System.out.printf("Разность диапазонов равна: [%f, %f]%n", from, to);
+        } else {
+            double from1 = intervalsDifference[0].getFrom();
+            double to1 = intervalsDifference[0].getTo();
+            double from2 = intervalsDifference[1].getFrom();
+            double to2 = intervalsDifference[1].getTo();
+
+            System.out.printf("Разность диапазонов равна: [%f, %f], [%f, %f]%n", from1, to1, from2, to2);
         }
 
         System.out.println();
 
-        range.setFrom(11.5);
-        range.setTo(12.6);
+        range1.setFrom(11.5);
+        range1.setTo(12.6);
+        range2.setFrom(12);
+        range2.setTo(13.7);
 
-        System.out.println("Новое начало диапазона равно: " + range.getFrom());
-        System.out.println("Новый конец диапазона равен: " + range.getTo());
+        System.out.println("Новое начало первого диапазона равно: " + range1.getFrom());
+        System.out.println("Новый конец первого диапазона равен: " + range1.getTo());
+        System.out.println("Новое начало второго диапазона равно: " + range2.getFrom());
+        System.out.println("Новый конец второго диапазона равен: " + range2.getTo());
 
-        System.out.println("Новая длина диапазона равна: " + range.getRangeLength());
+        System.out.println("Новая длина первого диапазона равна: " + range1.getRangeLength());
+        System.out.println("Новая длина второго диапазона равна: " + range2.getRangeLength());
 
-        if (range.isInside(number)) {
-            System.out.println("Число " + number + " принадлежит диапазону");
+        if (range1.isInside(number)) {
+            System.out.println("Число " + number + " принадлежит первому диапазону");
         } else {
-            System.out.println("Число " + number + " не принадлежит диапазону");
+            System.out.println("Число " + number + " не принадлежит первому диапазону");
+        }
+
+        if (range2.isInside(number)) {
+            System.out.println("Число " + number + " принадлежит второму диапазону");
+        } else {
+            System.out.println("Число " + number + " не принадлежит второму диапазону");
+        }
+
+        intervalsIntersection = range1.getIntervalsIntersection(range2);
+
+        if (intervalsIntersection == null) {
+            System.out.println("Диапазоны не пересекаются");
+        } else {
+            System.out.println("Диапазоны пересекаются в точках: " + Arrays.toString(intervalsIntersection));
+        }
+
+        intervalsUnion = range1.getIntervalsUnion(range1, range2);
+
+        if (intervalsUnion.length == 1) {
+            double from = intervalsUnion[0].getFrom();
+            double to = intervalsUnion[0].getTo();
+
+            System.out.printf("Объединение двух диапазонов: [%f, %f]%n", from, to);
+        } else {
+            double from1 = intervalsUnion[0].getFrom();
+            double to1 = intervalsUnion[0].getTo();
+            double from2 = intervalsUnion[1].getFrom();
+            double to2 = intervalsUnion[1].getFrom();
+
+            System.out.printf("Объединение двух диапазонов: [%f, %f], [%f, %f]%n", from1, to1, from2, to2);
+        }
+
+        intervalsDifference = range1.getIntervalsDifference(range1, range2);
+
+        if (intervalsDifference == null) {
+            System.out.println("Разность диапазонов равна нулю");
+        } else if (intervalsDifference.length == 1) {
+            double from = intervalsDifference[0].getFrom();
+            double to = intervalsDifference[0].getTo();
+
+            System.out.printf("Разность диапазонов равна: [%f, %f]%n", from, to);
+        } else {
+            double from1 = intervalsDifference[0].getFrom();
+            double to1 = intervalsDifference[0].getTo();
+            double from2 = intervalsDifference[1].getFrom();
+            double to2 = intervalsDifference[1].getTo();
+
+            System.out.printf("Разность диапазонов равна: [%f, %f], [%f, %f]%n", from1, to1, from2, to2);
         }
     }
 }
