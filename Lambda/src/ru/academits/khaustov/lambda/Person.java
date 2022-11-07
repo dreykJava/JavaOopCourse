b5636910575d3b1a8b5c1a83c83f0fae0586ac2c
Dreyk <i_khaustov@list.ru>
2022-11-07 23:52:19 +0700

    Исправления задачи Lambda

--- Lambda/src/ru/academits/khaustov/lambda/Person.java
+++ Lambda/src/ru/academits/khaustov/lambda/Person.java
@@ -1,12 +1,4 @@
 package ru.academits.khaustov.lambda;
 
 public record Person(String name, int age) {
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
 }
--- Lambda/src/ru/academits/khaustov/lambda_main/LambdaMain.java
+++ Lambda/src/ru/academits/khaustov/lambda_main/LambdaMain.java
@@ -15,10 +15,11 @@ public class LambdaMain {
                 new Person("Igor", 45),
                 new Person("Elisey", 45),
                 new Person("Maks", 40),
-                new Person("Ivan", 22));
+                new Person("Ivan", 22)
+        );
 
         List<String> distinctNames = persons.stream()
-                .map(Person::getName)
+                .map(Person::name)
                 .distinct()
                 .collect(Collectors.toList());
 
@@ -29,37 +30,34 @@ public class LambdaMain {
         System.out.println();
 
         List<Person> peopleUnder18List = persons.stream()
-                .filter(x -> x.getAge() < 18)
+                .filter(x -> x.age() < 18)
                 .collect(Collectors.toList());
 
-        OptionalDouble peopleUnder18AverageAges = peopleUnder18List.stream()
-                .mapToInt(Person::getAge)
+        OptionalDouble peopleUnder18AverageAge = peopleUnder18List.stream()
+                .mapToInt(Person::age)
                 .average();
 
-        if (peopleUnder18AverageAges.isPresent()) {
-            System.out.print("Средний возраст для людей младше 18 лет: ");
-            peopleUnder18AverageAges.ifPresent(System.out::print);
-            System.out.println(" лет.");
+        if (peopleUnder18AverageAge.isPresent()) {
+            System.out.println("Средний возраст для людей младше 18 лет: " + peopleUnder18AverageAge.getAsDouble() + " лет.");
         } else {
             System.out.println("Невозможно посчитать средний возраст.");
         }
 
         System.out.println();
-        
-        Map<String, Double> averageAgesByNames = persons.stream()
-                .collect(Collectors.groupingBy(Person::getName,
-                        Collectors.averagingInt(Person::getAge)));
 
-        averageAgesByNames.forEach((name, averageAges) ->
-                System.out.printf("Средний возраст для имени %s равен: %f%n", name, averageAges));
+        Map<String, Double> averageAgesByNames = persons.stream()
+                .collect(Collectors.groupingBy(Person::name, Collectors.averagingInt(Person::age)));
+
+        averageAgesByNames.forEach((name, averageAge) ->
+                System.out.printf("Средний возраст для имени %s равен: %f%n", name, averageAge));
         System.out.println();
 
-        String personsBetween20and45 = persons.stream()
-                .filter(x -> x.getAge() >= 20 && x.getAge() <= 45)
-                .sorted((p1, p2) -> p2.getAge() - p1.getAge())
-                .map(Person::getName)
+        String personsBetween20And45 = persons.stream()
+                .filter(x -> x.age() >= 20 && x.age() <= 45)
+                .sorted((p1, p2) -> p2.age() - p1.age())
+                .map(Person::name)
                 .collect(Collectors.joining(", ", "Имена людей в возрасте от 20 до 45 включительно: ", "."));
 
-        System.out.println(personsBetween20and45);
+        System.out.println(personsBetween20And45);
     }
 }
